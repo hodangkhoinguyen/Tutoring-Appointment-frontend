77bfd2f466e3abcf0684d64fabeb0f571bc95751
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2022-10-03 16:54:01 -0700

    complete sign up page

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,30 +1,107 @@
 import './style.css';
+import { useState } from 'react';
+import authService from '../../services/auth.js';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-    const loginHandler = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-    }
+    const navigate = useNavigate();
+    const signupHandler = (role) => {
+        if (password !== confirmPassword) {
+            alert('Confirmed password is not the same');
+        } else {
+            const user = {
+                role: role,
+                email: email,
+                password: password,
+                firstName: firstName,
+                lastName: lastName,
+            };
 
+            authService
+                .signup(user)
+                .then(() => {
+                    alert('Successfully sign up! You can log in now');
+                    navigate('/login');
+                })
+                .catch((err) => alert(err.response.data.message));
+        }
+    };
     return (
         <div className="login-box">
             <div className="login-container">
                 <div className="container grid wide">
                     <h2>Sign Up</h2>
-                    <input placeholder="Email" required></input>
-                    <input placeholder="Password" type="password" required></input>
-                    <input placeholder="Confirm Password" type="password" required></input>
+                    <input
+                        placeholder="First Name"
+                        onChange={(e) => {
+                            setFirstName(e.target.value);
+                        }}
+                        required
+                    ></input>
+                    <input
+                        placeholder="Last Name"
+                        onChange={(e) => {
+                            setLastName(e.target.value);
+                        }}
+                        required
+                    ></input>
+                    <input
+                        placeholder="Email"
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        required
+                    ></input>
+                    <input
+                        placeholder="Password"
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                        type="password"
+                        required
+                    ></input>
+                    <input
+                        placeholder="Confirm Password"
+                        onChange={(e) => {
+                            setConfirmPassword(e.target.value);
+                        }}
+                        type="password"
+                        required
+                    ></input>
                     <label>Sign up as:</label>
                     <div className="row">
-                        <button className="col l-6 m-6 c-12" onClick={loginHandler}>Student</button>
-                        <button className="col l-6 m-6 c-12" onClick={loginHandler}>Tutor</button>
+                        <button
+                            className="col l-6 m-6 c-12"
+                            onClick={() => {
+                                signupHandler('student');
+                            }}
+                        >
+                            Student
+                        </button>
+                        <button
+                            className="col l-6 m-6 c-12"
+                            onClick={() => {
+                                signupHandler('tutor');
+                            }}
+                        >
+                            Tutor
+                        </button>
                     </div>
                     <div className="action">
-                        <label>Have an account? <a href="/login">Log In</a></label>                        
-                    </div>                
+                        <label>
+                            Have an account? <a href="/login">Log In</a>
+                        </label>
+                    </div>
                 </div>
-            </div>            
+            </div>
         </div>
-    )
+    );
 }
 
 export default Login;
--- src/services/auth.js
+++ src/services/auth.js
@@ -5,6 +5,11 @@ class AuthService {
         let result = await http.post('auth/login', data);
         return result.data;
     }
+
+    async signup(data) {
+        let result = await http.post('auth/signup', data);
+        return result.data;
+    }
 }
 
 export default new AuthService();
